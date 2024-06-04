82d557229f5275bb3f40f092a8da831ab9177a3e
Ahmed <ahmedqss120@gmail.com>
2024-06-04 08:00:50 +0300

    Remove swapjs from FilterBadges

--- src/components/FilterBadges.js
+++ src/components/FilterBadges.js
@@ -2,42 +2,19 @@
 
 import Badge from './Badge';
 import filter from '@/lib/filter';
-import SwapIt from './SwapIt';
 
 const FilterBadges = ({ searchParams }) => {
   const params = new URLSearchParams(searchParams);
   return (
-    <>
-      <span className="hidden sm:block">
-        <Badge
-          title="الأحدث"
-          link={`?${filter('createdAt', 'DESC', params)}`}
-        />
-        <Badge title="الأشهر" link={`?${filter('views', 'DESC', params)}`} />
-        <Badge title="الأقدم" link={`?${filter('createdAt', 'ASC', params)}`} />
-        <Badge
-          title="الأكثر مشاركة"
-          link={`?${filter('shares', 'DESC', params)}`}
-        />
-      </span>
-      <section className="sm:hidden -ml-[.75rem]">
-        <SwapIt>
-          <Badge
-            title="الأحدث"
-            link={`?${filter('createdAt', 'DESC', params)}`}
-          />
-          <Badge title="الأشهر" link={`?${filter('views', 'DESC', params)}`} />
-          <Badge
-            title="الأقدم"
-            link={`?${filter('createdAt', 'ASC', params)}`}
-          />
-          <Badge
-            title="الأكثر مشاركة"
-            link={`?${filter('shares', 'DESC', params)}`}
-          />
-        </SwapIt>
-      </section>
-    </>
+    <span>
+      <Badge title="الأحدث" link={`?${filter('createdAt', 'DESC', params)}`} />
+      <Badge title="الأشهر" link={`?${filter('views', 'DESC', params)}`} />
+      <Badge title="الأقدم" link={`?${filter('createdAt', 'ASC', params)}`} />
+      <Badge
+        title="الأكثر مشاركة"
+        link={`?${filter('shares', 'DESC', params)}`}
+      />
+    </span>
   );
 };
 
