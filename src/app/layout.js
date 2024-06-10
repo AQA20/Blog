f5288a81409d72c76dad0e41bfc6839d5a32b23d
Ahmed <ahmedqss120@gmail.com>
2024-06-10 14:31:04 +0300

    Fix metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,6 @@ import { Noto_Sans_Arabic } from 'next/font/google';
 import { Suspense } from 'react';
 import { ThemeProvider } from 'next-themes';
 import CookiesDialog from '@/components/CookiesDialog';
-import Head from 'next/head';
 
 // If loading a variable font, you don't need to specify the font weight
 const noto_sans_arabic = Noto_Sans_Arabic({
@@ -17,6 +16,33 @@ const noto_sans_arabic = Noto_Sans_Arabic({
   variable: '--font-noto-sans-arabic',
 });
 
+const webDesc =
+  'استكشف أحدث المقالات والاكتشافات في مجالات اللياقة البدنية والتغذية الصحية عبر موقعنا. نقدم معلومات علمية موثوقة ونصائح عملية لتحقيق أهدافك الصحية والرياضية بأفضل الطرق.';
+
+export const metadata = {
+  title: '500Kalima موقع',
+  description: webDesc,
+  openGraph: {
+    // Open Graph (OG) tags
+    title: 'تصفح اخر المقالات',
+    description: webDesc,
+    url: 'https://500kalima.com/',
+    siteName: '500kalima',
+    images: [
+      {
+        url: 'https://500kalima.com/default-featured-img.png', // Must be an absolute URL
+        width: 680,
+        height: 510,
+      },
+    ],
+    locale: 'ar_AR',
+    type: 'website',
+  },
+  canonicalUrl: 'https://500kalima.com/',
+  keywords: 'صحة,لياقة,تمارين,غذاء صحي',
+  language: 'ar',
+};
+
 export default async function RootLayout({ children }) {
   const { articles } = await fetchArticles({
     orderBy: 'views',
@@ -24,86 +50,40 @@ export default async function RootLayout({ children }) {
     limit: 3,
   });
 
-  const article = articles[0];
-  const webDesc =
-    'استكشف أحدث المقالات والاكتشافات في مجالات اللياقة البدنية والتغذية الصحية عبر موقعنا. نقدم معلومات علمية موثوقة ونصائح عملية لتحقيق أهدافك الصحية والرياضية بأفضل الطرق.';
-
-  // Generate metadata dynamically based on fetched article data
-  const metadata = {
-    title: '500Kalima موقع',
-    description: webDesc,
-    openGraph: {
-      // Open Graph (OG) tags
-      title: article.title,
-      description: webDesc,
-      url: 'https://500kalima.com/',
-      siteName: '500kalima',
-      images: [
-        {
-          url: article.featuredImg, // Must be an absolute URL
-          width: 680,
-          height: 510,
-        },
-      ],
-      locale: 'ar_AR',
-      type: 'website',
-    },
-    canonicalUrl: 'https://500kalima.com/',
-    keywords: 'صحة,لياقة,تمارين,غذاء صحي',
-    language: 'ar',
-  };
-
   return (
-    <>
-      <Head>
-        {/* Metadata */}
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Open Graph (OG) tags */}
-        <meta property="og:title" content={metadata.ogTitle} />
-        <meta property="og:description" content={metadata.ogDescription} />
-        <meta property="og:image" content={metadata.ogImage} />
-        <meta property="og:url" content={metadata.ogUrl} />
-        {/* Other metadata */}
-        <link rel="canonical" href={metadata.canonicalUrl} />
-        <meta name="author" content={metadata.author} />
-        <meta name="keywords" content={metadata.keywords} />
-        <meta name="language" content={metadata.language} />
-      </Head>
-      <html
-        className={`pt-4 scroll-smooth ${noto_sans_arabic.variable}`}
-        lang="ar"
-        dir="rtl"
-        suppressHydrationWarning
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <body className="w-full xl:w-[680px] font-noto bg-light-surface dark:bg-dark-surface">
-            <main className="md:flex min-height">
-              <section className="w-full">
-                <Suspense>
-                  <Navbar />
-                </Suspense>
-                <div className="px-3 xl:w-[680px]">
-                  <section className="mb-6">
-                    <Suspense>{children}</Suspense>
-                  </section>
-                </div>
-              </section>
-              <aside className="hidden xl:block">
-                <Suspense>
-                  <Sidebar articles={articles} />
-                </Suspense>
-              </aside>
-            </main>
-            <div className="px-3">
+    <html
+      className={`pt-4 scroll-smooth ${noto_sans_arabic.variable}`}
+      lang="ar"
+      dir="rtl"
+      suppressHydrationWarning
+    >
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <body className="w-full xl:w-[680px] font-noto bg-light-surface dark:bg-dark-surface">
+          <main className="md:flex min-height">
+            <section className="w-full">
               <Suspense>
-                <Footer />
+                <Navbar />
               </Suspense>
-            </div>
-            <CookiesDialog />
-          </body>
-        </ThemeProvider>
-      </html>
-    </>
+              <div className="px-3 xl:w-[680px]">
+                <section className="mb-6">
+                  <Suspense>{children}</Suspense>
+                </section>
+              </div>
+            </section>
+            <aside className="hidden xl:block">
+              <Suspense>
+                <Sidebar articles={articles} />
+              </Suspense>
+            </aside>
+          </main>
+          <div className="px-3">
+            <Suspense>
+              <Footer />
+            </Suspense>
+          </div>
+          <CookiesDialog />
+        </body>
+      </ThemeProvider>
+    </html>
   );
 }
--- src/components/FilterBadges.js
+++ src/components/FilterBadges.js
@@ -1,5 +1,6 @@
 import Badge from './Badge';
 import filter from '@/lib/filter';
+import SwapIt from '@/components/SwapIt';
 
 const FilterBadge = ({ title, link }) => {
   return (
@@ -12,25 +13,48 @@ const FilterBadge = ({ title, link }) => {
 const FilterBadges = ({ urlSearchParams }) => {
   const params = new URLSearchParams(urlSearchParams);
   return (
-    <div className="flex flex-wrap gap-2">
-      <FilterBadge
-        title="الأحدث"
-        link={`?${filter('createdAt', 'DESC', params)}`}
-        className="pr-0"
-      />
-      <FilterBadge
-        title="الأشهر"
-        link={`?${filter('views', 'DESC', params)}`}
-      />
-      <FilterBadge
-        title="الأقدم"
-        link={`?${filter('createdAt', 'ASC', params)}`}
-      />
-      <FilterBadge
-        title="الأكثر مشاركة"
-        link={`?${filter('shares', 'DESC', params)}`}
-      />
-    </div>
+    <>
+      <div className="hidden md:flex flex-wrap gap-2">
+        <FilterBadge
+          title="الأحدث"
+          link={`?${filter('createdAt', 'DESC', params)}`}
+          className="pr-0"
+        />
+        <FilterBadge
+          title="الأشهر"
+          link={`?${filter('views', 'DESC', params)}`}
+        />
+        <FilterBadge
+          title="الأقدم"
+          link={`?${filter('createdAt', 'ASC', params)}`}
+        />
+        <FilterBadge
+          title="الأكثر مشاركة"
+          link={`?${filter('shares', 'DESC', params)}`}
+        />
+      </div>
+      <div className="md:hidden  gap-2">
+        <SwapIt slidesPerView={1.5}>
+          <FilterBadge
+            title="الأحدث"
+            link={`?${filter('createdAt', 'DESC', params)}`}
+            className="pr-0"
+          />
+          <FilterBadge
+            title="الأشهر"
+            link={`?${filter('views', 'DESC', params)}`}
+          />
+          <FilterBadge
+            title="الأقدم"
+            link={`?${filter('createdAt', 'ASC', params)}`}
+          />
+          <FilterBadge
+            title="الأكثر مشاركة"
+            link={`?${filter('shares', 'DESC', params)}`}
+          />
+        </SwapIt>
+      </div>
+    </>
   );
 };
 
