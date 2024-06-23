438bf2e77817ac88426cf00cdc5d163e24b76edf
Ahmed <ahmedqss120@gmail.com>
2024-06-23 20:45:26 +0300

    Comment out the download & unzip from s3 line

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,12 +8,12 @@ source ~/.nvm/nvm.sh
 nvm use v22.2.0
 
 
-echo "S3_BUCKET_NAME: $S3_BUCKET"
+# echo "S3_BUCKET_NAME: $S3_BUCKET"
 
-# Download and unzip from S3
-aws s3 cp s3://$S3_BUCKET/500kalima.zip /tmp/500kalima.zip
-unzip /tmp/500kalima.zip -d /home/ubuntu/500kalima
-rm -rf /tmp
+# # Download and unzip from S3
+# aws s3 cp s3://$S3_BUCKET/500kalima.zip /tmp/500kalima.zip
+# unzip /tmp/500kalima.zip -d /home/ubuntu/500kalima
+# rm -rf /tmp
 
 # Navigate to the application directory
 cd /home/ubuntu/500kalima
