9683cabb3dcd73dffd9879bde9ad6fc8f9b3dd27
Ahmed <ahmedqss120@gmail.com>
2024-11-10 23:47:20 +0300

    Secure users seeder sample data

--- server/seeders/2024.02.26T07.10.20.users.mjs
+++ server/seeders/2024.02.26T07.10.20.users.mjs
@@ -1,19 +1,11 @@
 import bcrypt from 'bcryptjs';
-import { readFileAsync } from '../utils/fsUtils.js';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import { handleAsyncError } from '../utils/handleErrors.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const fullPath = path.join(__dirname, '/samples/users.json');
+import users from './samples/users.js';
 
 export const up = handleAsyncError(async ({ context: { sequelize } }) => {
   const queryInterface = sequelize.getQueryInterface();
-  let userSamples = await readFileAsync(fullPath);
-  const usersData = JSON.parse(userSamples);
   const processedUsersData = await Promise.all(
-    usersData.users.map(async (user) => {
+    users.map(async (user) => {
       user.password = await bcrypt.hash(user.password, 10);
       return user;
     }),
