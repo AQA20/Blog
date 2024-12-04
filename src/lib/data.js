7f1711d5be44484970ffa1e65875bbb8932d86d3
Ahmed <ahmedqss120@gmail.com>
2024-12-04 16:41:31 +0300

    Remove react caching for fetchArticle to debug update issue

--- src/lib/data.js
+++ src/lib/data.js
@@ -20,17 +20,14 @@ const handleAsyncError = (func) => {
   };
 };
 
-// Using Cache for caching the previous calls, when the function is called again
-// with same arguments, wrap it with handleAsyncError to catch errors and fetch
+// Wrap it with handleAsyncError to catch errors and fetch
 // the article by its slug
-export const fetchArticle = cache(
-  handleAsyncError(async (slug) => {
-    const {
-      data: { data },
-    } = await apiClient.get(`/article/${slug}`);
-    return data;
-  }),
-);
+export const fetchArticle = handleAsyncError(async (slug) => {
+  const {
+    data: { data },
+  } = await apiClient.get(`/article/${slug}`);
+  return data;
+});
 
 // Using Cache for caching the previous calls, when the function is called again
 // with same arguments, wrap it with handleAsyncError to catch errors and fetch
