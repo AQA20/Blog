87dd588564f5f0e8ec049bc0d5e2964f6bce4ebc
Ahmed <ahmedqss120@gmail.com>
2024-12-15 16:08:50 +0300

    Fix config is undefine din databaseConfig

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -15,7 +15,7 @@ const config = {
     dialect: 'mysql',
     migrationStorageTableName: 'migrations',
   },
-  test: {
+  testing: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
