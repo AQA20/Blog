569ae37aa2b2f246642648e8dd12f8c0a315361d
Ahmed <ahmedqss120@gmail.com>
2024-12-20 20:20:25 +0300

    Fix cookie domain for cross-subdomain authentication

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -2,22 +2,8 @@ export const ACCESS_TOKEN_EXPIRATION_TIME = '15m';
 export const REFRESH_TOKEN_EXPIRATION_TIME = '7d';
 export const ACCESS_TOKEN_MAX_AGE_MS = 15 * 60 * 1000;
 export const REFRESH_TOKEN_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
-let domain;
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    domain = '.localhost';
-    break;
-  case 'production':
-    domain = 'admin.500kalima.com';
-    break;
-  case 'testing':
-    domain = 'manage.500kalima.com';
-    break;
-}
-
-export const DOMAIN = domain;
-
+export const DOMAIN =
+  process.env.NODE_ENV === 'development' ? '.localhost' : '.500kalima.com';
 export const SECURE = process.env.NODE_ENV === 'development' ? false : true;
 //For production environments when using cross-site requests over HTTPS.
 //'SameSite=None' allows the cookie to be sent in cross-site requests, and
