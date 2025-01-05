724019ac95df04d5807cb674d466f2bf8a705c94
Ahmed <ahmedqss120@gmail.com>
2025-01-05 10:40:17 +0300

    Refactor PM2 configuration to use 'npm' with arguments for starting applications; remove log file specifications to streamline setup.

--- server/pm2.config.cjs
+++ server/pm2.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_NAME || 'server-dev',
-      script: 'npm start',
+      script: 'npm',
+      args: 'start',
       // Group environment variables by purpose
       env: {
         PORT: process.env.PORT || '8080',
@@ -12,10 +13,7 @@ module.exports = {
       instances: 1, // Limit to 1 instance (since I have a single core)
       exec_mode: 'cluster', // Enable cluster mode, even with 1 worker
       max_memory_restart: '100M', // Restart the app if memory usage exceeds 100MB
-      node_args: '--max-old-space-size=256', // Set the maximum heap size for Node.js
-      log_file: './logs/pm2.log', // Specify log file location
-      out_file: './logs/out.log', // Specify stdout log file
-      error_file: './logs/error.log', // Specify stderr log file
+      node_args: '--max-old-space-size=256', // Set the maximum heap size for Node.jse
       log_date_format: 'YYYY-MM-DD HH:mm:ss', // Specify log date format
       autorestart: true, // Enable automatic restart on failure
       watch: false, // Disable watching for file changes (to reduce CPU usage)
