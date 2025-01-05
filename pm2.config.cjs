3d88c2eaf0bab94f4ea5d11ab6d6572000aa5d44
Ahmed <ahmedqss120@gmail.com>
2025-01-05 12:13:16 +0300

    Refactor PM2 configuration to use 'npm start' and disable automatic restart

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -2,17 +2,17 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_NAME || 'blog-dev',
-      script: 'npm',
-      args: 'start',
+      script: 'npm start',
       env: {
         PORT: process.env.CLIENT_PORT || '4000',
       },
       // PM2 specific configurations
       instances: 1, // Limit to 1 instance (since I have a single core)
+      exec_mode: 'cluster', // Enable cluster mode, even with 1 worker
       max_memory_restart: '100M', // Restart the app if memory usage exceeds 100MB
       node_args: '--max-old-space-size=256', // Set the maximum heap size for Node.js
       log_date_format: 'YYYY-MM-DD HH:mm:ss', // Specify log date format
-      autorestart: true, // Enable automatic restart on failure
+      autorestart: false, // Disable automatic restart on failure
       watch: false, // Disable watching for file changes (to reduce CPU usage)
     },
   ],
--- server/pm2.config.cjs
+++ server/pm2.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_NAME || 'server-dev',
-      script: 'npm',
-      args: 'start',
+      script: 'npm start',
       // Group environment variables by purpose
       env: {
         PORT: process.env.PORT || '8080',
@@ -11,10 +10,11 @@ module.exports = {
 
       // PM2 specific configurations
       instances: 1, // Limit to 1 instance (since I have a single core)
+      exec_mode: 'cluster', // Enable cluster mode, even with 1 worker
       max_memory_restart: '100M', // Restart the app if memory usage exceeds 100MB
       node_args: '--max-old-space-size=256', // Set the maximum heap size for Node.jse
       log_date_format: 'YYYY-MM-DD HH:mm:ss', // Specify log date format
-      autorestart: true, // Enable automatic restart on failure
+      autorestart: false, // Disable automatic restart on failure
       watch: false, // Disable watching for file changes (to reduce CPU usage)
     },
   ],
