dc3f808eba988b717a236f6fee94dcb4f988a0c0
Ahmed <ahmedqss120@gmail.com>
2025-08-15 21:46:36 +0300

    Fix type causing a syntax error

--- src/lib/data.js
+++ src/lib/data.js
@@ -22,42 +22,42 @@ const handleAsyncError = (func) => {
 
 // wrap it with handleAsyncError to catch errors and fetch
 // the article by its slug
-export const fetchArticle = c
-  handleAsyncError(async (slug) => {
-    const {
-      data: { data },
-    } = await apiClient.get(`/article/${slug}`);
-    return data;
-  });
+export const fetchArticle = handleAsyncError(async (slug) => {
+  const {
+    data: { data },
+  } = await apiClient.get(`/article/${slug}`);
+  return data;
+});
 
 // wrap it with handleAsyncError to catch errors and fetch
 // the all the articles
-export const fetchArticles = 
-  handleAsyncError(async (options) => {
-    const { orderBy, order, search, limit, page } = options;
-    const normalizedPage = page ? page : 1;
-    const normalizedOrderBy = orderBy ? orderBy : 'createdAt';
-    const normalizedOrder = order ? order : 'DESC';
-    const normalizedLimit = limit ? limit : 5;
-    const normalizedSearch = search ? search : '';
-    const {
-      data: { data },
-    } = await apiClient.get(
-      `/articles?orderBy=${normalizedOrderBy}&order=${normalizedOrder}&search=${normalizedSearch}&limit=${normalizedLimit}&page=${normalizedPage}`,
-    );
+export const fetchArticles = handleAsyncError(async (options) => {
+  const { orderBy, order, search, limit, page } = options;
+  const normalizedPage = page ? page : 1;
+  const normalizedOrderBy = orderBy ? orderBy : 'createdAt';
+  const normalizedOrder = order ? order : 'DESC';
+  const normalizedLimit = limit ? limit : 5;
+  const normalizedSearch = search ? search : '';
+  const {
+    data: { data },
+  } = await apiClient.get(
+    `/articles?orderBy=${normalizedOrderBy}&order=${normalizedOrder}&search=${normalizedSearch}&limit=${normalizedLimit}&page=${normalizedPage}`,
+  );
 
-    return data;
-  });
+  return data;
+});
 
 // Wrap it with handleAsyncError to catch errors and fetch
 // all of article slugs
-export const fetchArticleSlugs = cache(handleAsyncError(async () => {
-  const {
-    data: { data },
-  } = await apiClient.get('/article/slugs');
+export const fetchArticleSlugs = cache(
+  handleAsyncError(async () => {
+    const {
+      data: { data },
+    } = await apiClient.get('/article/slugs');
 
-  return data;
-}));
+    return data;
+  }),
+);
 
 export const fetchSuggestions = handleAsyncError(async (search) => {
   const term = search || '';
@@ -78,27 +78,31 @@ export const fetchTags = cache(
   }),
 );
 
-export const fetchTagArticles = cache(handleAsyncError(async (tag, options) => {
-  const { orderBy, order, page } = options;
-  const normalizedOrderBy = orderBy ? orderBy : 'createdAt';
-  const normalizedOrder = order ? order : 'DESC';
-  const normalizedPage = page ? page : 1;
-  const url = `/tag/${tag}/articles?orderBy=${normalizedOrderBy}&order=${normalizedOrder}&page=${normalizedPage}`
-  const res = await fetch(url, { next: { revalidate: 60 } });
-  const json = await res.json();
-  return json.data.data;
-}));
+export const fetchTagArticles = cache(
+  handleAsyncError(async (tag, options) => {
+    const { orderBy, order, page } = options;
+    const normalizedOrderBy = orderBy ? orderBy : 'createdAt';
+    const normalizedOrder = order ? order : 'DESC';
+    const normalizedPage = page ? page : 1;
+    const url = `/tag/${tag}/articles?orderBy=${normalizedOrderBy}&order=${normalizedOrder}&page=${normalizedPage}`;
+    const res = await fetch(url, { next: { revalidate: 60 } });
+    const json = await res.json();
+    return json.data.data;
+  }),
+);
 
 export const updateArticleShare = handleAsyncError(async (id) => {
   await apiClient.put(`/article/share/${id}`);
 });
 
-export const fetchRelatedArticles = handleAsyncError(async (articleId, categoryId, tags) => {
-  const tagIds = tags.map((tag) => tag.id).join(',');
-  const {
-    data: { data },
-  } = await apiClient.get(
-    `/articles/related/${articleId}?categoryId=${categoryId}&tagIds=${tagIds}`,
-  );
-  return data;
-});
+export const fetchRelatedArticles = handleAsyncError(
+  async (articleId, categoryId, tags) => {
+    const tagIds = tags.map((tag) => tag.id).join(',');
+    const {
+      data: { data },
+    } = await apiClient.get(
+      `/articles/related/${articleId}?categoryId=${categoryId}&tagIds=${tagIds}`,
+    );
+    return data;
+  },
+);
